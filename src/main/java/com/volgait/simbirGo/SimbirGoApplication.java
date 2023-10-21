3b9bc414274a28b28f974cfbb41000637366ab96
Danil_Malin <malin-03@mail.ru>
2023-10-21 13:44:27 +0400

    First commit

---
+++ src/main/java/com/volgait/simbirGo/SimbirGoApplication.java
@@ -0,0 +1,13 @@
+package com.volgait.simbirGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbirGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volgait/simbirGo/SimbirGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volgait.simbirGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
